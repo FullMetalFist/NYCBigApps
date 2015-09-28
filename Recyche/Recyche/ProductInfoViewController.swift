859990e8741053051ca31fd1d0574bc821884b1d
tintin2000 <gary@MacBook-Pro.local>
2015-09-28 16:57:34 -0400

    added the JSON GET
    added the JSON GET for searchupc.com

--- Recyche/Recyche/ProductInfoViewController.swift
+++ Recyche/Recyche/ProductInfoViewController.swift
@@ -7,29 +7,39 @@
 //
 
 import UIKit
+import Alamofire
 
-class ProductInfoViewController: UIViewController {
+let URLString = "http://www.searchupc.com/handlers/upcsearch.ashx?request_type=3"
+let access_token = "C6D5DA80-A126-4235-A35A-26E73FC64C2F"
+let UPC_code =  "037000088806"
 
+
+
+class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        Alamofire.request(.GET, URLString, parameters: ["access_token" : access_token ,"upc": UPC_code])
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+        }
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resour  ces that can be recreated.
     }
-    */
-
+    
+    
 }
+
